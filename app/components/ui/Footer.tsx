b03fbda9d967d592f4815d7b932fa543b601b137
jackretterer <jack.retterer@gmail.com>
2024-09-29 22:49:46 -0700

    Added Footer & Header

---
+++ app/components/ui/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 bg-black z-100">
+      <span className="ml-2 text-xl font-semibold text-gray-300">R.A. Scheuring</span>
+    </footer>
+  )
+}
